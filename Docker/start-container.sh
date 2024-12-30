4077c649bcc3536534acc43a7f354b920f25b5b9
Oliver <osnieho@gmail.com>
2024-12-30 16:12:39 +0100

    Changed permissions of squeezeboxuser und removed usage of nogroup

--- Docker/start-container.sh
+++ Docker/start-container.sh
@@ -6,10 +6,10 @@ PUID=${PUID:-`id -u squeezeboxserver`}
 PGID=${PGID:-`id -g squeezeboxserver`}
 
 usermod -o -u "$PUID" squeezeboxserver
-groupmod -o -g "$PGID" nogroup
+groupmod -o -g "$PGID" squeezeboxserver
 
 #Add permissions
-chown -R squeezeboxserver:nogroup /config /playlist
+chown -R squeezeboxserver:squeezeboxserver /config /playlist
 
 if [[ -f /config/custom-init.sh ]]; then
 	echo "Running custom initialization script..."
@@ -20,4 +20,4 @@ echo Starting Lyrion Music Server on port $HTTP_PORT...
 if [[ -n "$EXTRA_ARGS" ]]; then
 	echo "Using additional arguments: $EXTRA_ARGS"
 fi
-su squeezeboxserver -c '/usr/bin/perl /lms/slimserver.pl --prefsdir /config/prefs --logdir /config/logs --cachedir /config/cache --httpport $HTTP_PORT $EXTRA_ARGS'
+su squeezeboxserver -s /bin/sh -c '/usr/bin/perl /lms/slimserver.pl --prefsdir /config/prefs --logdir /config/logs --cachedir /config/cache --httpport $HTTP_PORT $EXTRA_ARGS'
