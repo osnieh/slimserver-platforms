a7ef1752232492335da79b04b705ef1e9e9ab2ce
Oliver Sniehotta <osnieho@gmail.com>
2025-01-15 00:41:37 +0100

    Create squeezeboxserver as systemuser, i.e. without loginshell.

--- Docker/start-container.sh
+++ Docker/start-container.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-#Set user and group
 umask 0002
 PUID=${PUID:-`id -u squeezeboxserver`}
 PGID=${PGID:-`id -g squeezeboxserver`}
 
+# Set uid of user squeezeboxserver to $PUID
+echo Set uid of user squeezeboxserver to $PUID
 usermod -o -u "$PUID" squeezeboxserver
+
+# Set id of group squeezeboxserver to $PGID and set gid of user squeezeboxserver to $PGID
+echo Set id of group squeezeboxserver to $PGID
 groupmod -o -g "$PGID" squeezeboxserver
+echo Set gid of user squeezeboxserver to $PGID
+usermod -g $PGID squeezeboxserver
 
 #Add permissions
 chown -R squeezeboxserver:squeezeboxserver /config /playlist
@@ -20,4 +26,4 @@ echo Starting Lyrion Music Server on port $HTTP_PORT...
 if [[ -n "$EXTRA_ARGS" ]]; then
 	echo "Using additional arguments: $EXTRA_ARGS"
 fi
-su squeezeboxserver -c '/usr/bin/perl /lms/slimserver.pl --prefsdir /config/prefs --logdir /config/logs --cachedir /config/cache --httpport $HTTP_PORT $EXTRA_ARGS'
+su squeezeboxserver -s /bin/sh -c '/usr/bin/perl /lms/slimserver.pl --prefsdir /config/prefs --logdir /config/logs --cachedir /config/cache --httpport $HTTP_PORT $EXTRA_ARGS'
