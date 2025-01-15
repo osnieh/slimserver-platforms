5e6c5ea869717f3cf8d55cd6c92ad87cf6a685f7
Oliver Sniehotta <osnieho@gmail.com>
2025-01-15 22:13:51 +0100

    Check of variables $PUID and $PGIG.

--- Docker/start-container.sh
+++ Docker/start-container.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
 
 umask 0002
-PUID=${PUID:-`id -u squeezeboxserver`}
-PGID=${PGID:-`id -g squeezeboxserver`}
 
-# Set uid of user squeezeboxserver to $PUID
-echo Set uid of user squeezeboxserver to $PUID
-usermod -o -u "$PUID" squeezeboxserver
+#
+# Check if $PUID is a number and in the range 1...59999 (i.e. root is not allowed)
+# Yes: Set uid of user squeezeboxserver to $PUID
+# No:  Set $PUID to the current uid of the user squeezeboxserver
+#
+
+if [[ $PUID =~ ^[0-9]+$ ]] && [ $PUID -ge 1 ] && [ $PUID -le 59999 ] ; then
+
+	# Print waring if $PUID is below 100
+	if [ $PUID -lt 100 ] ; then
+		echo Warning: PUID=$PUID is a reserved system user id.
+	fi
+   
+	echo Set uid of user squeezeboxserver to $PUID
+	usermod -o -u "$PUID" squeezeboxserver
+
+else
+	echo PUID=$PUID is not a number between 1 and 59999
+	PUID=$(id -u squeezeboxserver)
+	echo PUID is set to $PUID 
+fi
+
+
+#
+# Check if $PGID is a number and in the range 1...59999 (i.e. root is not allowed)
+# If not, set $PGID to the current gid of the user squeezeboxserver
+#
+
+if [[ $PGID =~ ^[0-9]+$ ]] && [ $PGID -ge 1 ] && [ $PGID -le 59999 ] ; then
+
+	# Print waring if $PUID is below 100
+	if [ $PGID -lt 100 ] ; then
+		echo Warning: PGID=$PGID is a reserved system group id.
+	fi
+else
+	echo PGID=$PGID is not a number between 1 and 59999
+	PGID=$(id -g squeezeboxserver)
+	echo  PGID is set to $PGID
+fi
+
 
 # Set id of group squeezeboxserver to $PGID and set gid of user squeezeboxserver to $PGID
 echo Set id of group squeezeboxserver to $PGID
